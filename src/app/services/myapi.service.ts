017cc36074d7dc1f1d35570175bfef1f0b6dc910
unknown <rlagurin.bos@gmail.com>
2020-03-30 23:16:23 +0800

    Update style

--- src/app/services/myapi.service.ts
+++ src/app/services/myapi.service.ts
@@ -6,6 +6,8 @@ import { HttpClient } from '@angular/common/http'
 export class myApiService{
     constructor(private httoclient: HttpClient){}
     getcomments(): Observable<any>{
-        return this.httoclient.get("https://coronavirus-ph-api.now.sh/cases");
+        // return this.httoclient.get("https://coronavirus-ph-api.now.sh/cases");
+        return this.httoclient.get("https://coronavirus-ph-api.herokuapp.com/cases");
+        
     }
 }
